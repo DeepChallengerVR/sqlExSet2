d25085e8a549498c989e65f13dce3670a088f8c1
Erik Borchers <eborchers@carpenter-11.elon.edu>
2016-11-28 09:40:49 -0500

    ex 1-3 attempted

--- exSet.sql
+++ exSet.sql
@@ -3,3 +3,13 @@ SELECT * FROM ap.terms
 INSERT INTO terms(terms_id, terms_description, terms_due_days)
 	VALUES (6, 'Net due 120 days', 120);
 
+-- ex2
+UPDATE terms
+SET terms_description = 'Net due 125 days',
+	terms_due_days = 125
+WHERE terms_id = 6;
+
+-- ex3
+DELETE FROM terms
+WHERE terms_id = 6;	
+
